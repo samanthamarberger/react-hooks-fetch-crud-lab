31792ce24e535e48f3923394062736b29c87cbad
samanthamarberger <slatham04@gmail.com>
2023-01-06 17:00:04 -0700

    Did the Fetch and most of the Post

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm(props, { onAddItem }) {
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -19,7 +19,29 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    const questionData = {
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4
+      ],
+      correctIndex: formData.correctIndex,
+    };
+    fetch("http://localhost:4000/questions" , {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: {
+        "prompt": questionData.prompt,
+        "answers": questionData.answers,
+        "correctIndex": questionData.correctIndex,
+      },
+    })
+    .then((r) => r.json())
+    .then((newItem) => onAddItem(newItem));
   }
 
   return (
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionForm from "./QuestionForm";
 
 function QuestionList() {
+  const [quizQuestions, setQuizQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then((r) => r.json())
+    .then((questions) => setQuizQuestions(questions));
+  }, [])
+
+  function handleAddQuestion(newQuestion) {
+    setQuizQuestions([...quizQuestions, newQuestion]);
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{quizQuestions.map((question) => <li>{question.prompt}</li>)}</ul>
+      {/* THIS IS WHERE THE DISPLAY ISSUE IS COMING FROM \/ */}
+      <QuestionForm onAddItem={handleAddQuestion}/>
     </section>
   );
 }
